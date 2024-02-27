2282c1dd99eb9eeff64e0092e2fc1e0c97860159
Mohammed Saif <113626584+Saifprogramm@users.noreply.github.com>
2024-02-27 01:33:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber" , "Software Developer" , "Full Stack Web Developer" , ""],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
